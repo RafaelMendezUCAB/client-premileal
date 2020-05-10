e7173fe707ca9a5f2f8c10cb6486e98b3c258a9a
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-05-10 15:59:52 -0400

    FASE001-003: Login Federado Creado

---
+++ src/views/Login.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-app id="inspire"
+           style="
+            background: #EEEEEE;
+           ">
+
+      <v-content>
+        <v-container
+          class="fill-height"
+          fluid
+        >
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-col
+              cols="12"
+              sm="8"
+              md="4"
+              align = "center"
+              justify="center"
+            >
+
+              <v-card class="elevation-3 px-6 py-8" 
+                      width="450px">
+
+              <span></span>
+              <!-- <div class="mx-auto text-center" >OR</div> -->
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn depressed @click="loginGoogle" width=100% color="#F4511E" class="white--text py-6 pb-10">
+                  <div class="pr-4 ml-n7 pl-4"><v-icon large left> mdi-google </v-icon></div>
+                  <div class=""> Log In with GOOGLE</div>
+                </v-btn>
+              </v-card-actions>
+
+              <v-card-actions>
+                <v-btn depressed @click="loginFacebook" width=100% color="#3B5998" class="white--text py-6 pb-10">
+                <div class="pr-4 ml-n3 pl-4"> <v-icon large left> mdi-facebook </v-icon></div>
+                <div class="ml-n1"> Log In with FACEBOOK</div>  
+                </v-btn>
+              </v-card-actions>
+
+            </v-card>
+           <div class=" text-center pt-4">
+             Don't have an account?
+               <router-link style="text-decoration: none;" 
+                            to="/Signup">
+                  Sign Up
+               </router-link>
+           </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+
+<script lang="ts">
+import {Vue} from 'vue-property-decorator'
+import Component from "vue-class-component";
+import { fa, providerGoogle, providerFacebook } from '../firebase';
+
+  @Component
+  export default class Login extends Vue{
+
+    data (){
+      return{
+        showPassword: false,
+        rules: {
+            required: (value: any) => !!value || 'Required.',
+            email: (value: any) => {
+              const pattern =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              return pattern.test(value) || 'Invalid e-mail.'
+            }
+         }
+      }
+    }
+
+    loginGoogle(){
+      fa.signInWithPopup(providerGoogle).then(result =>{
+        const token = result.credential
+        const user = result.user
+        console.log("datos del usuario",user);
+        console.log("token", token);
+      }).catch(error =>{
+        console.log(error);
+      })
+    }
+
+    loginFacebook(){
+      fa.signInWithPopup(providerFacebook).then(result => {
+        const token = result.credential
+      const user = result.user
+      console.log("datos del usuario",user);
+      console.log("token", token);
+      }).catch(error =>{
+        console.log(error);
+      })
+    }
+
+  }
+  
+</script>
