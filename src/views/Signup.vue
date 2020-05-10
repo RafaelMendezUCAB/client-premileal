77fd2bf13ff4cafe7c57e04fdce3cb0126cf9cb0
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-05-10 06:34:29 -0400

    FASE1-002: Registro no federado creado

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -27,40 +27,93 @@
                     <v-card class="elevation-3 px-6 py-8" 
                         width="450px">
                     
+                        <v-card-text>
+                            <v-form>
+                                <form v-on:submit="createUser">
+                                    <v-text-field
+                                        v-model="name"
+                                        prepend-icon= "mdi-account"
+                                        label="Name"
+                                        :rules="[rules.required, rules.alphabet]">
+                                    </v-text-field>
 
+                                    <v-text-field
+                                        v-model="lastName"
+                                        prepend-icon= "mdi-ab-testing"
+                                        label="Last Name"
+                                        :rules="[rules.required, rules.alphabet]">
+                                    </v-text-field>
 
+                                    <v-text-field
+                                        v-model="password"
+                                        class=""
+                                        prepend-icon= "mdi-lock"
+                                        label="Password"
+                                        :type= "showPassword1 ? 'text' : 'password'" 
+                                        hint="Password must contain at least eight characters, including at least 1 letter and 1 number."
+                                        persistent-hint
+                                        :append-icon= "showPassword1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                        @click:append="showPassword1 = !showPassword1">
+                                    </v-text-field>
 
+                                    <v-text-field
+                                        v-model="email"
+                                        prepend-icon= "mdi-at"
+                                        label="E-mail"
+                                        :rules="[rules.required, rules.email]">
+                                    </v-text-field>
 
+                                    <v-text-field
+                                        v-model="birthdate"
+                                        prepend-icon= "mdi-calendar-blank-outline"
+                                        label="Birth Date"
+                                        type="date">
+                                    </v-text-field>                
 
+                                    <v-btn depressed 
+                                        width=100% 
+                                        color="#0095ff" 
+                                        class="white--text py-3 pb-7">
+                                            Sign Up
+                                    </v-btn>
+
+                                </form>
+                            </v-form>
+                        </v-card-text>                    
 
                         <span></span>
-                        <!-- <div class="mx-auto text-center" >OR</div> -->
-                        <v-divider></v-divider>
-                        <v-card-actions>
 
+                        <div class="mx-auto text-center" >OR</div>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
                             <v-btn depressed @click="accessGoogle" width=100% color="#F4511E" class="white--text py-6 pb-10">
                                 <div class="pr-2 ml-n6 pl-4">
                                     <v-icon large left> mdi-google </v-icon>
                                 </div>
                                 <div class=""> Sign Up with GOOGLE</div>
                             </v-btn>
-
                         </v-card-actions>
 
-                        <v-card-actions>
-                            
+                        <v-card-actions>                            
                             <v-btn depressed @click="accessFacebook" width=100% color="#3B5998" class="white--text py-6 pb-10">
                                 <div class="pr-1 ml-n2 pl-4"> 
                                     <v-icon large left> mdi-facebook </v-icon>
                                 </div>
                                 <div class="ml-n1"> Sign Up with FACEBOOK</div>  
                             </v-btn>
-
                         </v-card-actions>
+                    </v-card>
 
-            </v-card>
-
-        </v-col>
+                    <div class="text-center pt-4">
+                        Already have an account?
+                            <router-link style="text-decoration: none;" 
+                                        to="/Login">
+                                Log in
+                            </router-link>
+                    </div>
+                </v-col>
             </v-row>
       </v-container>
   </v-content>
@@ -71,15 +124,37 @@
     
     import {Vue} from 'vue-property-decorator'
     import Component from "vue-class-component";
-    import { fb, db, fs } from '../firebase';
-    import { storage } from 'firebase';
+    // import { fb, db, fs } from '../firebase';
+    // import { storage } from 'firebase';
     import { fa, providerGoogle, providerFacebook } from '../firebase';
 
 @Component
 export default class Signup extends Vue{
     data(){
         return {
-
+            showPassword1: false,
+            password:'',
+            rules:{
+                required: (value: string[]) => !!value || 'Required.',
+                min: (v: string) => v.length >= 8 || 'Min 8 characters',
+                alphabet: (value: string) => {
+                    const pattern = /^[A-z]*$|^$/;
+                    return(
+                        pattern.test(value) ||"Only alphabet characters allowed"
+                    )
+                },
+                // counter: (value: any) => value.lenght <= 20 || 'Max 20 characters',        
+                password: (value: string) => {
+                    const pattern = /^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/;
+                    return (
+                        pattern.test(value) ||
+                        "Password must contain at least eight characters, including at least 1 letter and 1 number.");
+                },
+                email: (value: string) => {
+                    const pattern = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return pattern.test(value) || 'Invalid e-mail.'
+                }
+            },
         }
     }
 
@@ -105,14 +180,12 @@ export default class Signup extends Vue{
       })
     }
 
-    userFirstName = '';
-    userLastName = '';
-    userEmail = '';
-    userAlias = '';
-    userBirthday = '';
-    userPassword ='';
-    userDisplayName='';
-    mostrar = false;
+    name = '';
+    lastName = '';
+    password ='';
+    image ='';
+    email = '';
+    birthdate = '';
 
     async createUser(e: any){
         e.preventDefault();
@@ -121,13 +194,13 @@ export default class Signup extends Vue{
     
     getUser(){
       return {
-          userFirstName: this.userFirstName,
-          userLastName: this.userLastName,
-          userEmail: this.userEmail,
-          userAlias: this.userAlias,
-          userBirthdate: this.userBirthday,
-          userPassword: this.userPassword,
-          // imageUrl: this.imageUrl,
+          name: this.name,
+          lastName: this.lastName,
+          password: this.password,
+          image: this.image,
+          email: this.email,
+          birthdate: this.birthdate,
+          
       } 
   }
 
