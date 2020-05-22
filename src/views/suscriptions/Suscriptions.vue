3b2c941e9f0103b2ef1af535f6ffda293fb658c0
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-21 23:24:48 -0400

    FASE1-030: Creación de la estructura para implementar vista.

---
+++ src/views/suscriptions/Suscriptions.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-content>
+
+        </v-content>
+        <Footer></Footer>
+    </v-app>
+</template>
+
+<script lang='ts'>
+import {Vue, Watch} from 'vue-property-decorator'
+import Component from "vue-class-component";
+
+import Footer from '@/components/footer/Footer.vue';
+
+@Component({
+    components:{
+        Footer
+    }
+})
+export default class Suscriptions extends Vue{
+
+}
+</script>
+
+<style src="./Suscriptions.css" scoped>
+
+</style>
