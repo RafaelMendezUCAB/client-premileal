1699d800109a7eedd6547814b3b533b0b65eff12
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-21 21:39:19 -0400

    FASE1-016: Creada estructura inicial de la vista.

---
+++ src/views/points/purchase/PointsPurchase.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-content>
+
+        </v-content>
+        <Footer></Footer>
+    </v-app>
+</template>
+
+<script lang='ts'>
+import {Vue} from 'vue-property-decorator'
+import Component from "vue-class-component";
+
+import Footer from '@/components/footer/Footer.vue';
+
+@Component({
+    components:{
+        Footer
+    }
+})
+export default class PointsPurchase extends Vue{
+    
+}
+</script>
+
+<style src="./PointsPurchase.css" scoped></style>
