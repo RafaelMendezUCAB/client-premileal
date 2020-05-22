8f43202760f5f10a0aad10f262b57a42789c765e
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-21 22:34:52 -0400

    FASE1-020: Creada estructura para implementar vista de retiro de puntos.

---
+++ src/views/points/withdrawal/PointsWithdrawal.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-content>
+
+        </v-content>
+        <Footer></Footer>
+    </v-app>
+</template>
+
+<script>
+import {Vue} from 'vue-property-decorator'
+import Component from "vue-class-component";
+
+import Footer from '@/components/footer/Footer.vue';
+
+@Component({
+    components:{
+        Footer
+    }
+})
+export default class PointsWithdrawal extends Vue{
+    
+}
+</script>
+
+<style src="./PointsWithdrawal.css" scoped></style>
