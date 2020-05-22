a2dc45537eb76432f398e30a0f775e2d56c5b482
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-21 21:56:55 -0400

    FASE1-018: Creada estructura para implementar vista.

---
+++ src/views/suscriptions/premium/purchase/PremiumSuscriptionPurchase.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-content>
+            
+        </v-content>
+        <Footer></Footer>
+    </v-app>
+</template>
+
+<script lang='ts'>
+import {Vue} from 'vue-property-decorator'
+import Component from "vue-class-component";
+
+import Footer from '@/components/footer/Footer.vue';
+
+@Component({
+    components:{
+        Footer
+    }
+})
+export default class PremiumSuscriptionPurchase extends Vue{
+    
+}
+</script>
+
+<style src="./PremiumSuscriptionPurchase.css" scoped></style>
