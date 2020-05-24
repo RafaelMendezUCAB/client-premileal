2ced2240c4e22b57ec701ca804c20d2bbdafa493
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-23 23:07:01 -0400

    FASE1-026: Se ha creado metodo que envia peticion a servidor para validad cuenta bancaria.

--- src/services/API/bankAccount/bankAccountRequests.ts
+++ src/services/API/bankAccount/bankAccountRequests.ts
@@ -7,9 +7,12 @@ export default {
     /* ---------------------- POST METHODS -------------------------- */
     saveBankAccount(bankAccountData: any){
         return API_URL.post(process.env.VUE_APP_USER_BANK_ACCOUNT_ROUTE+"/create", bankAccountData);
-    }
+    },
 
     /* ---------------------- PUT METHODS -------------------------- */
+    verifyBankAccount(bankAccountID: number, data: any){
+        return API_URL.put(process.env.VUE_APP_USER_BANK_ACCOUNT_ROUTE+"/verify/"+bankAccountID, data);
+    }
   
     /* ---------------------- DELETE METHODS -------------------------- */
 }
