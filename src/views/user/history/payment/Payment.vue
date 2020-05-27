7b8e432f98c3cf176239d3c7bbe7c581023b55c5
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-05-27 02:18:33 -0400

    FASE1-028: Correccion de Enrutamiento a la pagina de historial de retiro

--- src/views/user/history/payment/Payment.vue
+++ src/views/user/history/payment/Payment.vue
@@ -332,7 +332,7 @@ export default class BankAccountStatus extends Vue{
     }
 
     gotoWithdrawalHistory(){
-      this.$router.push({name: 'userHistoryWithdrawal'});
+      this.$router.push({name: 'userWithdrawalHistory'});
     }
     
 }
